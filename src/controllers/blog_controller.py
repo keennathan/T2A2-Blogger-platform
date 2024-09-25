125b379b072d05222fce132278e47c1c77ad6c42
keennathan <keennathan@icloud.com>
2024-09-25 20:02:40 +1000

    finished blog controller

--- src/controllers/blog_controller.py
+++ src/controllers/blog_controller.py
@@ -8,7 +8,6 @@ from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from marshmallow import ValidationError
-from flask import abort
 
 # Blueprint for blogs
 blog_bp = Blueprint('blogs', __name__, url_prefix='/blogs')
@@ -18,7 +17,7 @@ blog_bp = Blueprint('blogs', __name__, url_prefix='/blogs')
 @jwt_required()
 def create_blog():
     try:
-        # Get the current user
+        # Get the current user from JWT
         current_user_id = get_jwt_identity()
         stmt = select(User).where(User.user_id == current_user_id)
         current_user = db.session.execute(stmt).scalar_one_or_none()
@@ -54,20 +53,44 @@ def create_blog():
         db.session.rollback()
         return jsonify({"error": str(e)}), 500
     
-# route to get all the published blogs
-@blog_bp.route('/', methods=['GET'])
+# route to filter by status of blogs
+@blog_bp.route('/status/<string:status>', methods=['GET'])
 @jwt_required()
-def get_published_blogs():
+def get_blogs_by_status(status):
     try:
-        # select to get only published blogs
-        stmt = select(Blogs).where(Blogs.status == 'published')
+        # select blogs by status
+        stmt = select(Blogs).where(Blogs.status == status)
         result = db.session.execute(stmt).scalars().all()
 
-        # If no published blogs are found
+        # If no blogs are found
         if not result:
-            return jsonify({"message": "No published blogs found"}), 404
+            return jsonify({"message": f"No blogs found with status '{status}'"}), 404
         
         return jsonify(blogs_schema.dump(result)), 200
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+# Route to list of blogs by a specific user
+@blog_bp.route('/user/<int:user_id>', methods=['GET'])
+@jwt_required()
+def get_blogs_by_user(user_id):
+    try:
+        # Select the user by user_id
+        stmt = select(User).where(User.user_id == user_id)
+        user = db.session.execute(stmt).scalar_one_or_none()
+
+        if not user:
+            return jsonify({"error": "User not found"}), 404
+        
+        # Select all the blogs of the user
+        stmt = select(Blogs).where(Blogs.user_id == user_id)
+        blogs = db.session.execute(stmt).scalars().all()
+
+        if not blogs:
+            return jsonify({"message": "No blogs where found for this user"}), 404
+        
+        return jsonify(blogs_schema.dump(blogs)), 200
     
     except Exception as e:
         return jsonify({"error": str(e)}), 500
@@ -89,3 +112,92 @@ def get_blog(blog_id):
     
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+# Route to update a blog (Only Author of the blog, Admin, Super Admin)   
+@blog_bp.route('/<int:blog_id>', methods=['PUT', 'PATCH'])
+@jwt_required()
+def update_blog(blog_id):
+    try:
+        # Get the user from JWT
+        current_user_id = get_jwt_identity()
+        stmt = select(User).where(User.user_id == current_user_id)
+        current_user = db.session.execute(stmt).scalar_one_or_none()
+
+        if not current_user:
+            return jsonify({"error": "User not found"}), 404
+        
+        # Get the blog to be updated
+        stmt = select(Blogs).where(Blogs.blog_id == blog_id)
+        blog = db.session.execute(stmt).scalar_one_or_none()
+
+        if not blog:
+            return jsonify({"error": "Blog not found"}), 404
+        
+        # Check the user is the author or an Admin or Super Admin
+        if blog.user_id != current_user.user_id and not (current_user.has_role("Admin") or current_user.has_role("Super Admin")):
+            return jsonify({"error": "You can only update your own blog or must be an Admin or Super Admin"}), 403
+        
+        # Parse and validate the request data
+        blog_data = blog_schema.load(request.get_json(), partial=True)
+
+        # update the blog
+        blog.title = blog_data.get("title", blog.title)
+        blog.content = blog_data.get("content", blog.content)
+        blog.status = blog_data.get("status", blog.status)
+
+        # Save the update
+        db.session.commit()
+        result = blog_schema.dump(blog)
+
+        return jsonify({"message": "Blog updated successfully", "blog": result}), 200
+    
+    except ValidationError as err:
+        return jsonify({"error": err.messages}), 400
+    except IntegrityError as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)}), 500
+    
+# Route to delete a blog (Only the Author or Admin or Super Admin)
+@blog_bp.route('/<int:blog_id>', methods=['DELETE'])
+@jwt_required()
+def delete_blog(blog_id):
+    try:
+        # Get the user from JWT
+        current_user_id = get_jwt_identity()
+        stmt = select(User).where(User.user_id == current_user_id)
+        current_user = db.session.execute(stmt).scalar_one_or_none()
+
+        if not current_user:
+            return jsonify({"error": "User not found"}), 404
+        
+        # Get the blog to be deleted
+        stmt = select(Blogs).where(Blogs.blog_id == blog_id)
+        blog = db.session.execute(stmt).scalar_one_or_none()
+
+        if not blog:
+            return jsonify({"error": "Blog not found"}), 404
+        
+        # get the Author of the blog
+        stmt = select(User).where(User.user_id == blog.user_id)
+        blog_author = db.session.execute(stmt).scalar_one_or_none()
+
+        # Check the user is the author 
+        if blog.user_id == current_user.user_id:
+            # Author can only delete thier own blog, not if is from an admin or super admin
+            if blog_author.has_role("Admin") or blog_author.has_role("Super Admin"):
+                return jsonify({"error": "You cannot delete a blog created by an Admin or Super Admin"}), 403
+            else:
+                db.session.delete(blog)
+                db.session.commit()
+                return jsonify({"message": "Blog deleted successfully"}), 200
+        # Admins and super admins can delete any blog
+        elif current_user.has_role("Admin") or current_user.has_role("Super Admin"):
+            db.session.delete(blog)
+            db.session.commit()
+            return jsonify({"message": "Blog deleted successfully"}), 200
+        else:
+            return jsonify({"error": "You can only delete your own blog or must be an Admin or Super Admin"}), 403
+        
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
